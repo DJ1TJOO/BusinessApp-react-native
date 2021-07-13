255bd4aafb58d2d2268fe1b2b4fd3a92a4c4fcbf
DJ1TJOO <t.ferb1@gmail.com>
2021-07-13 10:37:08 +0200

    Upload for testing

--- app/screen/RegisterScreen.js
+++ app/screen/RegisterScreen.js
@@ -10,9 +10,71 @@ import FormHeading from "../components/form/FormHeading";
 import FormDate from "../components/form/FormDate";
 import Colors from "../config/Colors";
 
+const validators = {
+	business_name: (formData, text) => {
+		if (text.length < 6) return "De bedrijfsnaam mag niet korter zijn dan 5 karakters";
+		if (text.length > 255) return "De bedrijfsnaam mag niet langer zijn dan 255 karakters";
+		return true;
+	},
+	firstname: (formData, text) => {
+		if (text.length < 6) return "De voornaam mag niet korter zijn dan 5 karakters";
+		if (text.length > 255) return "De voornaam mag niet langer zijn dan 255 karakters";
+		return true;
+	},
+	lastname: (formData, text) => {
+		if (text.length < 6) return "De achternaam mag niet korter zijn dan 5 karakters";
+		if (text.length > 255) return "De achternaam mag niet langer zijn dan 255 karakters";
+		return true;
+	},
+	account_email: (formData, text) => {
+		if (
+			!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+				text
+			)
+		)
+			return "Het email address is incorrect";
+
+		if (text.length > 255) return "Het email address mag niet langer zijn dan 255 karakters";
+		return true;
+	},
+	account_password: (formData, text) => {
+		if (text.length < 9) return "Het wachtwoord mag niet korter zijn dan 8 karakters";
+		if (text.length > 255) return "Het wachtwoord mag niet langer zijn dan 255 karakters";
+		if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*_-])(?=.{8,})/.test(text))
+			return "Het wachtwoord moet minimaal:\n - 1 hoofdletter \n - 1 kleineletter\n - 1 speciaalteken \n - 1 nummer bevatten";
+		return true;
+	},
+	account_confirm_password: (formData, text) => {
+		console.log(formData);
+		if (text !== formData.account_password) return "Het wachtwoord moet het zelfde zijn ";
+		return true;
+	},
+	account_born: (formData, currentDate) => {
+		const ageDiffMilliseconds = Date.now() - currentDate.getTime();
+		const ageDate = new Date(ageDiffMilliseconds);
+		const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+		if (age < 18) return "U moet minimaal 18 jaar oud zijn";
+		return true;
+	},
+	account_function: (formData, text) => {
+		if (text.length > 255) return "De functie omschrijving mag niet langer zijn dan 255 karakters";
+		return true;
+	},
+};
+
 const RegisterScreen = ({ navigation }) => {
 	const [formLayout, setFormLayout] = useState(null);
-	const [formData, setFormData] = useState({});
+	const [formData, setFormData] = useState({
+		business_name: "",
+		account_firstname: "",
+		account_lastname: "",
+		account_email: "",
+		account_password: "",
+		account_confirm_password: "",
+		account_born: new Date(),
+		account_function: "",
+	});
 	const [isLogoSet, setIsLogoSet] = useState(false);
 
 	const pickImage = async () => {
@@ -85,32 +147,20 @@ const RegisterScreen = ({ navigation }) => {
 					label="Bedrijfsnaam"
 					onChange={(text) => setFormData({ ...formData, business_name: text })}
 					textContentType="name"
-					validate={(text) => {
-						if (text.length < 6) return "De bedrijfsnaam mag niet korter zijn dan 5 karakters";
-						if (text.length > 255) return "De bedrijfsnaam mag niet langer zijn dan 255 karakters";
-						return true;
-					}}
+					validate={validators.business_name.bind(undefined, formData)}
 				/>
 				<FormHeading title="Hoofdaccount" />
 				<FormInput
 					label="Voornaam"
 					onChange={(text) => setFormData({ ...formData, firstname: text })}
 					textContentType="name"
-					validate={(text) => {
-						if (text.length < 6) return "De voornaam mag niet korter zijn dan 5 karakters";
-						if (text.length > 255) return "De voornaam mag niet langer zijn dan 255 karakters";
-						return true;
-					}}
+					validate={validators.firstname.bind(undefined, formData)}
 				/>
 				<FormInput
 					label="Achternaam"
 					onChange={(text) => setFormData({ ...formData, lastname: text })}
 					textContentType="name"
-					validate={(text) => {
-						if (text.length < 6) return "De achternaam mag niet korter zijn dan 5 karakters";
-						if (text.length > 255) return "De achternaam mag niet langer zijn dan 255 karakters";
-						return true;
-					}}
+					validate={validators.lastname.bind(undefined, formData)}
 				/>
 				<FormInput
 					label="Email"
@@ -121,51 +171,25 @@ const RegisterScreen = ({ navigation }) => {
 						})
 					}
 					textContentType="emailAddress"
-					validate={(text) => {
-						if (
-							!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
-								text
-							)
-						)
-							return "Het email address is incorrect";
-
-						if (text.length > 255) return "Het email address mag niet langer zijn dan 255 karakters";
-						return true;
-					}}
+					validate={validators.account_email.bind(undefined, formData)}
 				/>
 				<FormInput
 					label="Wachtwoord"
 					hideText={true}
 					onChange={(text) => setFormData({ ...formData, account_password: text })}
 					textContentType="password"
-					validate={(text) => {
-						if (text.length < 9) return "Het wachtwoord mag niet korter zijn dan 8 karakters";
-						if (text.length > 255) return "Het wachtwoord mag niet langer zijn dan 255 karakters";
-						if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*_-])(?=.{8,})/.test(text))
-							return "Het wachtwoord moet minimaal:\n - 1 hoofdletter \n - 1 kleineletter\n - 1 speciaalteken \n - 1 nummer bevatten";
-						return true;
-					}}
+					validate={validators.account_password.bind(undefined, formData)}
 				/>
 				<FormInput
 					label="Bevestig wachtwoord"
 					hideText={true}
 					onChange={(text) => setFormData({ ...formData, account_confirm_password: text })}
 					textContentType="password"
-					validate={(text) => {
-						if (text !== formData.account_password) return "Het wachtwoord moet het zelfde zijn ";
-						return true;
-					}}
+					validate={validators.account_confirm_password.bind(undefined, formData)}
 				/>
 				<FormDate
 					onChange={(currentDate) => setFormData({ ...formData, account_born: currentDate })}
-					validate={(currentDate) => {
-						const ageDiffMilliseconds = Date.now() - currentDate.getTime();
-						const ageDate = new Date(ageDiffMilliseconds);
-						const age = Math.abs(ageDate.getUTCFullYear() - 1970);
-
-						if (age < 18) return "U moet minimaal 18 jaar oud zijn";
-						return true;
-					}}
+					validate={validators.account_born.bind(undefined, formData)}
 					label="Geboorte datum"
 					links={[
 						{
@@ -205,10 +229,7 @@ const RegisterScreen = ({ navigation }) => {
 					label="Functie omschrijving"
 					onChange={(text) => setFormData({ ...formData, account_function: text })}
 					textContentType="jobTitle"
-					validate={(text) => {
-						if (text.length > 255) return "De functie omschrijving mag niet langer zijn dan 255 karakters";
-						return true;
-					}}
+					validate={validators.account_function.bind(undefined, formData)}
 				/>
 				{formData.image && (
 					<Image
@@ -229,6 +250,15 @@ const RegisterScreen = ({ navigation }) => {
 				<FormButton
 					onPress={() => {
 						//TODO: register
+						for (const key in formData) {
+							console.log(key);
+							if (validators.hasOwnProperty(key)) {
+								const value = validators[key].bind(undefined, formData)(formData[key]);
+								if (value !== true) return;
+							}
+						}
+
+						console.log("image check");
 					}}
 				>
 					Registeren
