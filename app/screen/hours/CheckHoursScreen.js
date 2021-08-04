05f41c9ad6e0846c2e19979631768e5dc67e2ce3
DJ1TJOO <t.ferb1@gmail.com>
2021-08-05 00:07:15 +0200

    Added data to check hours

--- app/screen/hours/CheckHoursScreen.js
+++ app/screen/hours/CheckHoursScreen.js
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { StyleSheet } from "react-native";
 
 import Heading from "../../components/Heading";
 import Wrapper from "../../components/Wrapper";
 import MenuCard from "../../components/menu/MenuCard";
 
+import dataContext from "../../contexts/dataContext";
+
+import config from "../../config/config";
+
 const CheckHoursScreen = ({ navigation }) => {
+	const [currentError, setCurrentError] = useState(null);
+
+	const [data, setData] = useContext(dataContext);
+
+	useEffect(() => {
+		// Get user names
+		if (!data.checkHours) data.checkHours = {};
+		(async () => {
+			try {
+				const res = await fetch(config.api + "users/business/" + data.user.businessId).then((res) => res.json());
+				if (res.success) data.checkHours.users = res.data;
+				else data.checkHours.users = [];
+				setData({ ...data });
+			} catch (error) {
+				throw error;
+			}
+		})();
+	}, []);
+
 	return (
-		<Wrapper navigation={navigation} showHeader={true}>
+		<Wrapper navigation={navigation} showHeader={true} error={currentError}>
 			<Heading title="Uren controleren" style={styles.heading} />
-			<MenuCard
-				title="Full Name"
-				onPress={() => {
-					//TODO: data
-					navigation.navigate("CheckHoursPerson");
-				}}
-			/>
-			<MenuCard
-				title="Full Name 1"
-				onPress={() => {
-					//TODO: data
-					navigation.navigate("CheckHoursPerson");
-				}}
-			/>
-			<MenuCard
-				title="Full Name 2"
-				onPress={() => {
-					//TODO: data
-					navigation.navigate("CheckHoursPerson");
-				}}
-			/>
-			<MenuCard
-				title="Full Name 3"
-				onPress={() => {
-					//TODO: data
-					navigation.navigate("CheckHoursPerson");
-				}}
-			/>
+			{data.checkHours &&
+				data.checkHours.users &&
+				data.checkHours.users.map((user) => (
+					<MenuCard
+						key={user.id}
+						title={user.first_name + " " + user.last_name}
+						onPress={() => {
+							navigation.navigate("CheckHoursPerson", { id: user.id });
+						}}
+					/>
+				))}
 		</Wrapper>
 	);
 };
