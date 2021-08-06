5cb897d440aa65783b5147df5ee073a85f0f81cb
DJ1TJOO <t.ferb1@gmail.com>
2021-08-06 22:21:09 +0200

    Changed from else to replace
    Signed-off-by: DJ1TJOO <t.ferb1@gmail.com>

--- app/languages/utils.js
+++ app/languages/utils.js
@@ -13,29 +13,41 @@ const convertError = (language, res, body, type, fields) => {
 
 	if (type) {
 		errorMessage = errorMessage.replace("<type>", type);
-	} else {
-		errorMessage = errorMessage.replace("<type>", "");
 	}
 
-	if (res.data) {
+	if (res.data && res.data.field) {
 		errorMessage = errorMessage.replace("<field>", fields[res.data.field] || res.data.field);
 		errorMessage = errorMessage.replace("<value>", `'${body[res.data.field]}'`);
-		errorMessage = errorMessage.replace("<min>", res.data.min);
-		errorMessage = errorMessage.replace("<max>", res.data.max);
-		errorMessage = errorMessage.replace("<minLength>", res.data.minLength);
-		errorMessage = errorMessage.replace("<maxLength>", res.data.maxLength);
-		errorMessage = errorMessage.replace("<minSize>", res.data.minSize);
-		errorMessage = errorMessage.replace("<maxSize>", res.data.maxSize);
-	} else {
-		errorMessage = errorMessage.replace("<field>", "");
-		errorMessage = errorMessage.replace("<value>", "");
-		errorMessage = errorMessage.replace("<min>", "");
-		errorMessage = errorMessage.replace("<max>", "");
-		errorMessage = errorMessage.replace("<minLength>", "");
-		errorMessage = errorMessage.replace("<maxLength>", "");
-		errorMessage = errorMessage.replace("<minSize>", "");
-		errorMessage = errorMessage.replace("<maxSize>", "");
 	}
+	if (res.data && res.data.min) {
+		errorMessage = errorMessage.replace("<min>", res.data.min);
+	}
+	if (res.data && res.data.max) {
+		errorMessage = errorMessage.replace("<max>", res.data.max);
+	}
+	if (res.data && res.data.minLength) {
+		errorMessage = errorMessage.replace("<minLength>", res.data.minLength);
+	}
+	if (res.data && res.data.maxLength) {
+		errorMessage = errorMessage.replace("<maxLength>", res.data.maxLength);
+	}
+	if (res.data && res.data.minSize) {
+		errorMessage = errorMessage.replace("<minSize>", res.data.minSize);
+	}
+	if (res.data && res.data.maxSize) {
+		errorMessage = errorMessage.replace("<maxSize>", res.data.maxSize);
+	}
+
+	// Replace none replaces
+	errorMessage = errorMessage.replace("<type>", "");
+	errorMessage = errorMessage.replace("<field>", "");
+	errorMessage = errorMessage.replace("<value>", "");
+	errorMessage = errorMessage.replace("<min>", "");
+	errorMessage = errorMessage.replace("<max>", "");
+	errorMessage = errorMessage.replace("<minLength>", "");
+	errorMessage = errorMessage.replace("<maxLength>", "");
+	errorMessage = errorMessage.replace("<minSize>", "");
+	errorMessage = errorMessage.replace("<maxSize>", "");
 
 	// Remove double spaces
 	errorMessage = errorMessage.replace(/  +/g, " ");
